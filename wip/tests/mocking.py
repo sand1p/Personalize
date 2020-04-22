3c3cee5b93b47dd7781185991a77aceb3c752812
Sandip Gatkal <sandip@ridecell.com>
2020-04-22 16:32:14 +0530

    File-Removal as a Service with Mock Patch

--- wip/tests/mocking.py
+++ wip/tests/mocking.py
@@ -1,18 +1,20 @@
-from wip.tests.mymodule import rm
+from wip.tests.mymodule import RemovalService
 
 from unittest import mock
 from unittest import TestCase
 
 
-class RunTestCase(TestCase):
+class RemovalServiceTestCase(TestCase):
 
     @mock.patch('wip.tests.mymodule.os.path')
     @mock.patch('wip.tests.mymodule.os')
     def test_rm(self, mock_os, mock_path):
+
         # set up the mock
+        reference = RemovalService
         mock_path.isfile.return_value = False
 
-        rm("any path")
+        reference.rm("any path")
 
         # test that the remove call was not called
         self.assertFalse(mock_os.remove.called)
@@ -20,7 +22,7 @@ class RunTestCase(TestCase):
         # make the  file 'exist'
         mock_path.isfile.return_value = True
 
-        rm("any path")
+        reference.rm("any path")
 
         mock_os.remove.assert_called_with("any path")
 
--- wip/tests/mymodule.py
+++ wip/tests/mymodule.py
@@ -1,8 +1,11 @@
 import os
+import os.path
 
 
-def rm(filename):
-    if os.path.isfile(filename):
-        os.remove(filename)
+class RemovalService(object):
+    """A service for removing objects from the filesystem."""
 
+    def rm(filename):
+        if os.path.isfile(filename):
+            os.remove(filename)
 
